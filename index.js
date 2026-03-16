6794bce3d1703ac0234a9f35a9d8f791de7f0196
Lruking <107301713+Lruking@users.noreply.github.com>
2026-03-16 11:09:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 {
   "name": "discord-readme-bot",
   "main": "index.js",
+  "scripts": {
+    "postinstall": "node deploy-commands.js"
+  },
   "dependencies": {
-    "discord.js": "^14.14.1"
+    "discord.js": "^14.14.1",
+    "dotenv": "^16.0.0"
   }
 }
